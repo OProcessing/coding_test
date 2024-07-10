7440d945019e2fcf528bb98cac8f8ecec6bf11ca
Han <157330660+OProcessing@users.noreply.github.com>
2024-07-11 02:42:58 +0900

    [level 2] Title: 짝지어 제거하기, Time: 83.80 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    
+    stack = []
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    
+    if stack : return 0
+    else : return 1  
